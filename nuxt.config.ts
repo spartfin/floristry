f71c2d48213e76e71878bb069b47f560f034d9fb
Aleksey Goryn <fin.07@bk.ru>
2023-04-18 15:32:15 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ['~/assets/scss/main.scss'],
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
+                        @import "@/assets/scss/_mixins.scss";
+                        @import "@/assets/scss/_variables.scss";
+                    `
+                },
+            },
+        },
+    },
+})
